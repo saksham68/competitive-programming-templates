ad6721f48ec214c8fbd24587c32ca85449adbffe
SAKSHAMOP3000 <135740303+SAKSHAMOP3000@users.noreply.github.com>
2025-10-21 22:10:17 +0530

    Create stack.cpp

---
+++ monotonic/stack.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+
+struct monotonic_stack {
+	stack<pair<int,int>> s;
+
+	void push(int x) {
+		s.push((s.empty() ? make_pair(x,x): make_pair(x,min(x,s.top().second))));
+	}
+
+	void pop() {
+		s.pop();
+	}
+
+	int minimum() {
+		return s.top().second;
+	}
+};
+
+int32_t main() {
+
+	monotonic_stack s;
+
+   while(true) {
+      int n;
+      cin >> n;
+      if(n == 1) {
+      	// insert
+      	int a;
+      	cin >> a;
+      	s.push(a);
+      } else if(n == 2) {
+      // remove
+      	s.pop();
+      } else if(n == 69) {
+      	return 0;
+      } else {
+      	// find minimum 
+      	cout << s.minimum();
+      }
+   }
+
+   return 0;
+	
+}
